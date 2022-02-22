d73434dc565dd4111a98e57dd25feec4e5268744
OrganicAC <anthonychaplin17@gmail.com>
2022-02-22 19:35:00 +0000

    refactored and passed statement_spec test

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,10 +1,7 @@
 require 'statement'
 
 describe Statement do
-
-  subject(:statement) { described_class.new }
-  let(:transaction_test) { double(:transaction, date: 21/02/2022, credit: '2000', debit: '', balance: '2000') }
-
+  
   describe '.header' do
     it 'displays the header of the statement' do
       expect(subject.header).to eq 'Date || Credit || Debit || Balance'
@@ -12,9 +9,17 @@ describe Statement do
   end
 
   it 'should print the transaction array' do
-    transaction = '21/02/2022 || 2000 ||  || 2000'
+    
+    transaction_test = instance_double(Transaction)
+    allow(transaction_test).to receive(:format).and_return("#{test_time_format} || 2000 ||  || 2000")
+
+    transaction = "Date || Credit || Debit || Balance\n#{test_time_format} || 2000 ||  || 2000\n"
     bank_statement = [transaction_test]
-    expect { statement.print_statement(bank_statement) }
+    expect { subject.print_statement(bank_statement) }
       .to output(transaction).to_stdout
   end
+end
+
+def test_time_format
+  Time.now.strftime('%d/%m/%Y')
 end
